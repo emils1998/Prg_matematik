e26ecbed274e06c965c3d1bc87522ad5f64757cb
scem14 <emils3508@gmail.com>
2016-09-16 11:24:41 +0200

    2nd commint

--- src/Övning.java
+++ src/Övning.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
  */
 public class Övning {
 
-//Skapa Frågerna
+    //Skapa Frågerna
     static int[] q1;
     static int[] q2;
+    static int[] svar;
 
     public static void main(String[] args) {
         cQuestions();
@@ -18,23 +19,40 @@ public class Övning {
 
     public static void cQuestions() {
         Random n = new Random();
-        g1 = new int (10);
-        g2 = new int (10);
-        for (int i = 0 ; i < 10 ; i++) {
-                q1(i) = n.nextInt(1000);
-                q2(i) = n.nextInt(1000);
+        q1 = new int[10];
+        q2 = new int[10];
+        for (int i = 0; i < 10; i++) {
+            q1[i] = n.nextInt(1000);
+            q2[i] = n.nextInt(1000);
         }
     }
-//Ställa Frågorna (1 thru 10)
-    public static void aQuestions(){
+
+    //Ställa Frågorna (1 thru 10)
+    public static void aQuestions() {
+        svar = new int[10];
         Scanner in = new Scanner(System.in);
-        svar = new int(10);
-        for (int i = 0 ; i < 10 ; i++;)
-
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Vad är " + q1[i] + " + " + q2[i] + " ? ");
+            svar[i] = in.nextInt();
+        }
     }
+
+
+
 //Rätta Svar
-    public static void coQuestions(){
+    public static void coQuestions() {
+        int poäng = 0;
+        for (int i = 0 ; i < 10 ; i++) {
+            if(q1[i] + q2[i] == svar[i]) {
+                System.out.println(q1[i] + " + " + q2[i] + " + " + svar[i] + "(Rätt)");
+                    poäng += 10;
+                }
 
-    }
-}
+                else{
+                    System.out.println(q1[i] + " + " + q2[i] + " = " + svar[i] + "(" + "Fel, det rätta svaret är " + (q1[i] + q2[i]) + ")");
+                }
+            }
+            System.out.println("\nTotal poäng: " + poäng);
+        }
+        }
+
